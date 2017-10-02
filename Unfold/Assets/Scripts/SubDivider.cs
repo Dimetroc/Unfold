c7b4ce7c4cb3dc487bb8cecea3e774aebb8c5c1b
Dmytro Aleksiuk <d.aleksiuk@ameria.de>
2017-10-02 17:00:46 +0300

    - working on data structure

---
+++ Unfold/Assets/Scripts/SubDivider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Unfold
+{
+    public class SubDivider
+    {
+        private Mesh _startMesh;
+
+        public SubDivider(Mesh startMesh)
+        {
+            _startMesh = startMesh;
+        }
+    }
+}
